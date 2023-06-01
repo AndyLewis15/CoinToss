855e498bd49cc2990eb574f475b4f27344c4c59f
Andy Lewis <lewisa71@ferris.edu>
2023-05-31 22:42:05 -0400

    Displays statistics of results

--- src/CoinToss.java
+++ src/CoinToss.java
@@ -1,26 +1,35 @@
 import java.util.Random;
 import java.util.Scanner;
 public class CoinToss {
-    static Scanner scnr = new Scanner(System.in);
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
         Random random = new Random();
+
         System.out.println("Enter how many flips you want:");
         int userTosses = scnr.nextInt();
 
         int numTosses = userTosses;
 
+        int headsCount = 0;
+        int tailsCount = 0;
 
 
         for (int i = 0; i < numTosses; i++) {
             int result = random.nextInt(2); // 0 represents heads, 1 represents tails
 
-
             if (result == 0) {
-                System.out.println("Toss " + (i + 1) + ": Heads");
+                headsCount++;
             } else {
-                System.out.println("Toss " + (i + 1) + ": Tails");
+                tailsCount++;
             }
+
+
+            System.out.println("Toss " + (i + 1) + ": " + (result == 0 ? "Heads" : "Tails"));
         }
+
+        // Print the statistics
+        System.out.println("Number of Heads: " + headsCount);
+        System.out.println("Number of Tails: " + tailsCount);
     }
 }
