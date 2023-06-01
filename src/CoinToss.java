6658b973c2904d51c035ddbb802d0de1d777d0d9
Andy Lewis <lewisa71@ferris.edu>
2023-05-31 22:40:51 -0400

    Adds ability for user to decide how many times to flip the coin. Displays all results

--- src/CoinToss.java
+++ src/CoinToss.java
@@ -1,18 +1,26 @@
 import java.util.Random;
-
+import java.util.Scanner;
 public class CoinToss {
+    static Scanner scnr = new Scanner(System.in);
     public static void main(String[] args) {
-        // Create an instance of the Random class
+
         Random random = new Random();
+        System.out.println("Enter how many flips you want:");
+        int userTosses = scnr.nextInt();
 
-        // Simulate a coin toss
-        int result = random.nextInt(2); // 0 represents heads, 1 represents tails
+        int numTosses = userTosses;
 
-        // Print the result
-        if (result == 0) {
-            System.out.println("Heads");
-        } else {
-            System.out.println("Tails");
+
+
+        for (int i = 0; i < numTosses; i++) {
+            int result = random.nextInt(2); // 0 represents heads, 1 represents tails
+
+
+            if (result == 0) {
+                System.out.println("Toss " + (i + 1) + ": Heads");
+            } else {
+                System.out.println("Toss " + (i + 1) + ": Tails");
+            }
         }
     }
 }
